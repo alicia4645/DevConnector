1837efe62af8e293e8f9adf3e45fb2fcffc6d2dd
alicia <team3229@robpress.ecs.soton.ac.uk>
2025-11-18 15:02:07 +0000

    Setting up react and the frontend

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,58 @@
+import React, { useState }  from 'react';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    });
+
+    const { email, password} = formData;
+
+    const onChange = e => setFormData({ ...formData, [e.target.name]:e.target.value});
+
+    const onSubmit = e => {
+        e.preventDefault();
+        console.log('logged in')
+
+    }
+
+    return (
+        <>
+            <h1 className="large text-primary">Sign In</h1>
+            <form className="form" onSubmit={e => onSubmit(e)}>
+                <div className="form-group">
+                    <input 
+                        type="email" 
+                        placeholder="Email Address"
+                        name="email"
+                        value={email} 
+                        onChange={e => onChange(e)} 
+                        required  
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        minLength="6"
+                        value={password} 
+                        onChange={e => onChange(e)} 
+                        required 
+                    />
+                </div>                
+                <input 
+                    type="submit" 
+                    className="btn btn-primary" 
+                    value="Login" 
+                />
+            </form>
+            <p className="my-1">
+                Need to create an account? <Link to="/Register">Sign Up</Link>
+            </p>
+        </>
+  )
+}
+
+export default Login;
