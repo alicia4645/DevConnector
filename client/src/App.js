4e37b629eea99f8fdd3d7892acc5fd5368e36238
alicia <team3229@robpress.ecs.soton.ac.uk>
2025-12-09 14:12:45 +0000

    redux setup and alerts

--- client/src/App.js
+++ client/src/App.js
@@ -4,23 +4,30 @@ import { Navbar } from './components/layout/Navbar';
 import { Landing } from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login  from './components/auth/Login';
+import Alert from './components/layout/Alert';
+// Redux 
+import { Provider } from 'react-redux';
+import store from './store';
 import './App.css';
 
 const App = () => 
-  <Router>
-    <>
-      <Navbar />
-      <Routes>
-        <Route exact path='/' element={ <Landing /> } />
-      </Routes> 
-      <section className='container'>
-          <Routes>
-            <Route exact path='/register' element={ <Register /> } />
-            <Route exact path='/login' element={ <Login /> } />
-          </Routes>
-      </section> 
-    </>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={ <Landing /> } />
+        </Routes> 
+        <section className='container'>
+          <Alert />
+            <Routes>
+              <Route exact path='/register' element={ <Register /> } />
+              <Route exact path='/login' element={ <Login /> } />
+            </Routes>
+        </section> 
+      </>
+    </Router>
+  </Provider>
  
 
 export default App;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,12 @@
 import React, { useState }  from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types'
 
-const Register = () => {
+const Register = ({
+    setAlert
+}) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -16,7 +21,7 @@ const Register = () => {
     const onSubmit = e => {
         e.preventDefault();
         if(password !== password2){
-            console.log('Passwords do not match');
+            setAlert('Passwords do not match', 'danger');
         }else{
             console.log(formData);
         }
@@ -86,4 +91,12 @@ const Register = () => {
     )
 }
 
-export default Register;
+Register.propTypes = {
+    setAlert: PropTypes.func.isRequired
+}
+
+const mapDispatchToProps = {
+    setAlert
+}
+
+export default connect(null, mapDispatchToProps)(Register);
